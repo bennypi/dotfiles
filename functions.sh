766cccfe1b73fd4d952cda8917df455961e6837f
pmania <pmania@tzi.de>
2013-11-18 22:03:58 +0100

    added PWD function to functions.sh

--- functions.sh
+++ functions.sh
@@ -6,3 +6,8 @@ function gcp {
 function cdl {
   cd $1;ll
 }
+# special PWD output, with only the last three directories
+function PWD {
+tmp=${PWD%/*/*/*};
+[ ${#tmp} -gt 0 -a "$tmp" != "$PWD" ] && echo ${PWD:${#tmp}+1} || echo $PWD;
+}
